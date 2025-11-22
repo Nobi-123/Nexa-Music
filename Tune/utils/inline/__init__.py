e9c04b9b84587a076d3de369f9bdc34accb302a4
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-22 09:02:23 +0530

    Initial commit

---
+++ Tune/utils/inline/__init__.py
@@ -0,0 +1,8 @@
+# Authored By Certified Coders — v1.2 (2025-11-14)
+from .extras import *
+from .help import *
+from .play import *
+from .queue import *
+from .settings import *
+from .start import *
+from .speed import *
