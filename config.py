343066a7b60af530a175a38b13de319a105da7ea
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-22 12:08:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_ID = int(getenv("API_ID", 27798659))
 API_HASH = getenv("API_HASH", "26100c77cee02e5e34b2bbee58440f86")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-OWNER_ID = int(getenv("OWNER_ID", 8449801101)
+OWNER_ID = int(getenv("OWNER_ID", 8449801101))
 OWNER_USERNAME = getenv("OWNER_USERNAME", "NonCarder")
 BOT_USERNAME = getenv("BOT_USERNAME", "NexaEraMusicBot")
 BOT_NAME = getenv("BOT_NAME", "˹ᴛᴜɴᴇ ᴠɪᴀ ʙᴏᴛ˼")
