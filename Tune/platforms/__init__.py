d1c583e8a4626e2a330f7c705f69e7be074c9fe1
Nobi-123 <nobita.infringement.report@gmail.com>
2025-11-22 09:01:26 +0530

    Initial commit

---
+++ Tune/platforms/__init__.py
@@ -0,0 +1,8 @@
+# Authored By Certified Coders — v1.2 (2025-11-14)
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
